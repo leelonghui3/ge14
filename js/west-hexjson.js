96724d6399f44c19586d539f90bb87318cfc48be
Lee Long Hui <leelonghui3@gmail.com>
2018-06-23 15:26:39 +0200

    done painting Kedah & Perlis

--- js/west-hexjson.js
+++ js/west-hexjson.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
       bottom: 10,
       left: 10
     },
-    width = 1200 - margin.left - margin.right,
+    width = 1400 - margin.left - margin.right,
     height = 900 - margin.top - margin.bottom;
 
   // Create the svg element
